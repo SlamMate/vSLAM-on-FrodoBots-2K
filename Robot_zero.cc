2ce7f4e73f7a7f397f805f143fd19820cdcffb95
Zhang Qi <96341196+SlamMate@users.noreply.github.com>
2024-07-06 21:33:26 +0200

    Add files via upload

---
+++ Robot_zero.cc
@@ -0,0 +1,153 @@
+/**
+* This file is part of ORB-SLAM3
+*
+* Copyright (C) 2017-2021 Carlos Campos, Richard Elvira, Juan J. Gómez Rodríguez, José M.M. Montiel and Juan D. Tardós, University of Zaragoza.
+* Copyright (C) 2014-2016 Raúl Mur-Artal, José M.M. Montiel and Juan D. Tardós, University of Zaragoza.
+*
+* ORB-SLAM3 is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+* License as published by the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* ORB-SLAM3 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+* the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along with ORB-SLAM3.
+* If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include<iostream>
+#include<algorithm>
+#include<fstream>
+#include<chrono>
+
+#include<opencv2/core/core.hpp>
+#include<opencv2/highgui/highgui.hpp>
+#include<opencv2/imgproc/imgproc.hpp>
+#include<opencv2/videoio.hpp>
+
+#include<System.h>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    if(argc != 4)
+    {
+        cerr << endl << "Usage: ./mono_custom path_to_vocabulary path_to_settings path_to_sequence" << endl;
+        return 1;
+    }
+
+    string videoFilePath = string(argv[3]) + "/recording/rgb.ts";
+
+    // Create SLAM system. It initializes all system threads and gets ready to process frames.
+    ORB_SLAM3::System SLAM(argv[1], argv[2], ORB_SLAM3::System::MONOCULAR, true);
+    float imageScale = SLAM.GetImageScale();
+
+    // Vector for tracking time statistics
+    vector<float> vTimesTrack;
+
+    // Open video file
+    cv::VideoCapture cap(videoFilePath);
+    if(!cap.isOpened())
+    {
+        cerr << "Failed to open video file: " << videoFilePath << endl;
+        return 1;
+    }
+
+    double fps = cap.get(cv::CAP_PROP_FPS);
+    double T = 1.0 / fps;
+    int nFrames = (int)cap.get(cv::CAP_PROP_FRAME_COUNT);
+    vTimesTrack.resize(nFrames);
+
+    cout << endl << "-------" << endl;
+    cout << "Start processing sequence ..." << endl;
+    cout << "Frames in the sequence: " << nFrames << endl << endl;
+
+    double t_resize = 0.f;
+    double t_track = 0.f;
+
+    cv::Mat im;
+    for(int ni=0; ni<nFrames; ni++)
+    {
+        cap >> im;
+        if(im.empty())
+        {
+            cerr << "Failed to load frame at: " << ni << endl;
+            break;
+        }
+
+        double tframe = ni * T;
+
+        if(imageScale != 1.f)
+        {
+#ifdef REGISTER_TIMES
+    #ifdef COMPILEDWITHC11
+            std::chrono::steady_clock::time_point t_Start_Resize = std::chrono::steady_clock::now();
+    #else
+            std::chrono::monotonic_clock::time_point t_Start_Resize = std::chrono::monotonic_clock::now();
+    #endif
+#endif
+            int width = im.cols * imageScale;
+            int height = im.rows * imageScale;
+            cv::resize(im, im, cv::Size(width, height));
+#ifdef REGISTER_TIMES
+    #ifdef COMPILEDWITHC11
+            std::chrono::steady_clock::time_point t_End_Resize = std::chrono::steady_clock::now();
+    #else
+            std::chrono::monotonic_clock::time_point t_End_Resize = std::chrono::monotonic_clock::now();
+    #endif
+            t_resize = std::chrono::duration_cast<std::chrono::duration<double,std::milli> >(t_End_Resize - t_Start_Resize).count();
+            SLAM.InsertResizeTime(t_resize);
+#endif
+        }
+
+#ifdef COMPILEDWITHC11
+        std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
+#else
+        std::chrono::monotonic_clock::time_point t1 = std::chrono::monotonic_clock::now();
+#endif
+
+        // Pass the image to the SLAM system
+        SLAM.TrackMonocular(im, tframe);
+
+#ifdef COMPILEDWITHC11
+        std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
+#else
+        std::chrono::monotonic_clock::time_point t2 = std::chrono::monotonic_clock::now();
+#endif
+
+#ifdef REGISTER_TIMES
+            t_track = t_resize + std::chrono::duration_cast<std::chrono::duration<double,std::milli> >(t2 - t1).count();
+            SLAM.InsertTrackTime(t_track);
+#endif
+
+        double ttrack = std::chrono::duration_cast<std::chrono::duration<double> >(t2 - t1).count();
+
+        vTimesTrack[ni] = ttrack;
+
+        // Wait to load the next frame
+        if(ttrack < T)
+            usleep((T - ttrack) * 1e6);
+    }
+
+    // Stop all threads
+    SLAM.Shutdown();
+
+    // Tracking time statistics
+    sort(vTimesTrack.begin(), vTimesTrack.end());
+    float totaltime = 0;
+    for(int ni = 0; ni < nFrames; ni++)
+    {
+        totaltime += vTimesTrack[ni];
+    }
+    cout << "-------" << endl << endl;
+    cout << "median tracking time: " << vTimesTrack[nFrames / 2] << endl;
+    cout << "mean tracking time: " << totaltime / nFrames << endl;
+
+    // Save camera trajectory
+    SLAM.SaveKeyFrameTrajectoryTUM("KeyFrameTrajectory.txt");
+
+    return 0;
+}
+
